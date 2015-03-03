d1be26360448761c6b781b1c9f0f57b10349721a
Stephania Cavadas <stephaniacc93@gmail.com>
2015-03-02 17:13:56 -0800

    First commit

---
+++ ReactiveAgent/ReactiveAgent/FabricaDeAcciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveAgent
+{
+    public class FabricaDeAcciones
+    {
+        private Estado Estado { get; set; }
+
+        public FabricaDeAcciones(Estado estado)
+        {
+            this.Estado = estado;
+        }
+
+        public void CrearUnaAccion()
+        {
+            if (Estado == Estado.Easy)
+            {
+                //haz algo
+            }
+            else if (Estado == Estado.Med)
+            {
+                //haz otra cosa
+            }
+            else if (Estado == Estado.Hard)
+            {
+                //haz algo mas
+            }
+        }
+
+    }
+}
---
+++ ReactiveAgent/ReactiveAgent/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveAgent
+{
+    public class Main
+    {
+
+        public Main(Estado estado)
+        {
+            //Entorno modifica estado
+            FabricaDeAcciones fabricaDeAcciones = new FabricaDeAcciones(estado);                
+        }
+    }
+}
